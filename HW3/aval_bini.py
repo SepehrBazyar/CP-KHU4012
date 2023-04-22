a71adfb0de97f3fdab2c31f80bbfd5c5db86695c
Sepehr Bazyar <bazyar.sepehr@gmail.com>
2023-04-22 17:22:04 +0330

    Create aval_bini.py

---
+++ HW3/aval_bini.py
@@ -0,0 +1,12 @@
+a = int(input())
+b = int(input())
+primes = []
+for i in range(a + 1, b):
+    for j in range(2, i):
+        if i % j == 0:
+            break
+    else:
+        if i > 1:
+            primes.append(str(i))
+
+print(",".join(primes))
